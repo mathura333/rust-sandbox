cdb47aefb071c3ec70fcf7e8360d7944761c8083
Mathura Das <mathuradas333@gmail.com>
2021-09-13 16:22:12 +0530

    Print

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,22 @@
 pub fn run() {
+  // * Print to console
   println!("Hello from print.rs file");
+
+  // * Basic formatting
+  println!("{} is from {}","foo","bar");
+
+  // * Positional Arguments
+  println!("{0} is from {1} and {0} likes to {2}","foo","bar","baar");
+
+  // * Named arguments
+  println!("{name} like to play {game}",name="foo",game="bar");
+
+  // * Placeholder traits
+  println!("Binary: {:b} Hex: {:x} Octal: {:o}",10,10,10);
+
+  // * Placeholder for debug trait
+  println!("{:?}",(12,true, "hello"));
+
+  // * Basic operation
+  println!("10 + 10 = {}",10+10);
 }
